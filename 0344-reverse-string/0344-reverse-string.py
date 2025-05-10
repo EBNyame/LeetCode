dba5994b71c1ef37b1d85ec2580f94d5e41633db
Exodus Blessed Nyame <ebnyame001@st.ug.edu.gh>
2025-05-10 09:46:24 +0000

    Time: 1 ms (58.01%), Space: 23 MB (83.08%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,12 +3,13 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        x = s[::-1]
-        s[:] = x 
+        # x = s[::-1]
+        # s[:] = x 
+        # print(ord('A'))
        
-        # pre, nex = 0, len(s)-1
-        # while pre <= nex:
-        #     s[pre], s[nex] = s[nex], s[pre]
-        #     pre += 1
-        #     nex -= 1
+        pre, nex = 0, len(s)-1
+        while pre <= nex:
+            s[pre], s[nex] = s[nex], s[pre]
+            pre += 1
+            nex -= 1
         
